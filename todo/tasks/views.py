1e7c4d1f23b093995a06fa7eb574ee50ffb4d53d
Zeel Shah <zshah6@asu.edu>
2023-04-22 22:34:03 -0700

    todo setup models

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+from .forms import *
+
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+
+    form = TaskForm()
+
+    context = {'tasks' : tasks, 'form' : form}
+    return render(request, 'tasks/list.html', context)
