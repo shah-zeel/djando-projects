2495d3e48ec75119339002865db84edd52ae92e0
Zeel Shah <zshah6@asu.edu>
2023-04-25 09:55:11 -0700

    todo_auth Delete View

--- todo_auth/base/urls.py
+++ todo_auth/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate     # '.' bez they are in the same file structure
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView     # '.' bez they are in the same file structure
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
 ]
