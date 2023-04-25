b03248e66fb557cda49f28b5d088c2f813860d11
Zeel Shah <zshah6@asu.edu>
2023-04-25 09:43:42 -0700

    todo_auth Update View

--- todo_auth/base/urls.py
+++ todo_auth/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate     # '.' bez they are in the same file structure
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate     # '.' bez they are in the same file structure
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
 ]
