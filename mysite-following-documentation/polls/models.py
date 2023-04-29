ceaac1fb69d951766242f9f2c6387e3e120c0170
Zeel Shah <zshah6@asu.edu>
2023-04-28 17:25:29 -0700

    mysite Customise Admin site

--- mysite-following-documentation/polls/models.py
+++ mysite-following-documentation/polls/models.py
@@ -5,6 +5,8 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.contrib import admin
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -15,6 +17,16 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
